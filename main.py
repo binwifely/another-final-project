707b7c3a00e5b625f982420c97ae6dfff23d6b64
binwifely <115625431+binwifely@users.noreply.github.com>
2023-06-09 23:15:07 +0300

    Add files via upload
    09.06.2023

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import*
 from random import random, shuffle
+from button import Button
 from sprite import Vehicle
 from sprite import Player
 
@@ -39,7 +40,24 @@ road = (100, 0, road_width, road_height)
 left_edge_marker = (90, 0, marker_width, road_height)
 right_edge_marker = (500, 0, marker_width, road_height)
 
-#road lines
+#road lanes
+class Line:
+    def __init__(self, x, start_y):
+        self.rects = []
+        self.start_y = start_y 
+
+        for i in range(14):
+            rect = Rect(x, self.start_y, 10, 50)
+            self.start_y += 150
+            self.rects.append(rect)
+
+    def draw(self):
+        for r in self.rects:
+            draw.rect(screen, white, r)
+            r.y += 0
+            if r.y >= 680:
+                r.y = -230
+
 
 lane1 = 150
 lane2 = 250
@@ -48,22 +66,32 @@ lane4 = 450
 
 lanes = [lane1, lane2, lane3, lane4]
 
-#creating player
+line_a = Line(lane1 + 50, -80)
+line_b = Line(lane2 + 50, -80)
+line_c = Line(lane3 + 50, -80)
 
-players = sprite.Group()
+'''lines = sprite.Group()
+lines.add(line_a, line_b, line_c)'''
+#creating player
 
 x_list = [150, 250, 350, 450]
 choose_x = [0, 1, 2, 3]
 shuffle(choose_x)
-player_y = 500
+player_y = 400
 
-player = Player(x_list[choose_x[0]], player_y)
-players.add(player)
+player = Player('car.png', x_list[choose_x[0]], player_y)
+
 #game initialization
 
 clock = time.Clock()
 fps = 120
 run = True
+
+
+#ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+#ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+
+
 while run:
 
     clock.tick(fps)
@@ -80,48 +108,12 @@ while run:
     draw.rect(screen, yellow, right_edge_marker)
 
     #draw lane markers
-    markers_move_y += speed * 2
-    if markers_move_y >= marker_height * 14:
-        markers_move_y = 30
-    for y in range(-marker_height + markers_move_change, marker_height - markers_move_change):
-
-        draw.rect(screen, white, (lane1 + 45, y + markers_move_y, marker_width, marker_height))
-        draw.rect(screen, white, (lane2 + 45, y + markers_move_y, marker_width, marker_height))
-        draw.rect(screen, white, (lane3 + 45, y + markers_move_y, marker_width, marker_height))
-
-        draw.rect(screen, white, (lane1 + 45, y + markers_move_y + 150, marker_width, marker_height))
-        draw.rect(screen, white, (lane2 + 45, y + markers_move_y + 150, marker_width, marker_height))
-        draw.rect(screen, white, (lane3 + 45, y + markers_move_y + 150, marker_width, marker_height))
-
-        draw.rect(screen, white, (lane1 + 45, y + markers_move_y + 300, marker_width, marker_height))
-        draw.rect(screen, white, (lane2 + 45, y + markers_move_y + 300, marker_width, marker_height)) 
-        draw.rect(screen, white, (lane3 + 45, y + markers_move_y + 300, marker_width, marker_height))
-
-        draw.rect(screen, white, (lane1 + 45, y + markers_move_y + 450, marker_width, marker_height))
-        draw.rect(screen, white, (lane2 + 45, y + markers_move_y + 450, marker_width, marker_height)) 
-        draw.rect(screen, white, (lane3 + 45, y + markers_move_y + 450, marker_width, marker_height))  
-
-        draw.rect(screen, white, (lane1 + 45, y + markers_move_y + 600, marker_width, marker_height))
-        draw.rect(screen, white, (lane2 + 45, y + markers_move_y + 600, marker_width, marker_height)) 
-        draw.rect(screen, white, (lane3 + 45, y + markers_move_y + 600, marker_width, marker_height))               
-
-        draw.rect(screen, white, (lane1 + 45, y + markers_move_y - 150, marker_width, marker_height))
-        draw.rect(screen, white, (lane2 + 45, y + markers_move_y - 150, marker_width, marker_height))
-        draw.rect(screen, white, (lane3 + 45, y + markers_move_y - 150, marker_width, marker_height))
-
-        draw.rect(screen, white, (lane1 + 45, y + markers_move_y - 300, marker_width, marker_height))
-        draw.rect(screen, white, (lane2 + 45, y + markers_move_y - 300, marker_width, marker_height)) 
-        draw.rect(screen, white, (lane3 + 45, y + markers_move_y - 300, marker_width, marker_height))
-
-        draw.rect(screen, white, (lane1 + 45, y + markers_move_y - 450, marker_width, marker_height))
-        draw.rect(screen, white, (lane2 + 45, y + markers_move_y - 450, marker_width, marker_height)) 
-        draw.rect(screen, white, (lane3 + 45, y + markers_move_y - 450, marker_width, marker_height))
-
-        draw.rect(screen, white, (lane1 + 45, y + markers_move_y - 600, marker_width, marker_height))
-        draw.rect(screen, white, (lane2 + 45, y + markers_move_y - 600, marker_width, marker_height))
-        draw.rect(screen, white, (lane3 + 45, y + markers_move_y - 600, marker_width, marker_height))
 
 
-        players.draw(screen)
-        
+    line_a.draw()
+    line_b.draw()
+    line_c.draw()
+
+    player.draw(screen)
+
     display.update()
--- sprite.py
+++ sprite.py
@@ -1,19 +1,25 @@
 from pygame import*
 
 class Vehicle(sprite.Sprite):
-    def __init__(self, image, x ,y):
+    def __init__(self, image_name, x ,y):
+        super().__init__()
 
-        #scale image
-        image_scale = 45 / image.get_rect().width
-        new_width = image.get_rect().width * image_scale
-        new_height = image.get_rect().height * image_scale
-        self.image = transform.scale(image, (new_width, new_height))
-
+        self.image = image.load(image_name)
         self.rect = self.image.get_rect()
+        
+        #scale image
+        image_scale = 45 / self.rect.width
+        new_width = self.rect.width * round(image_scale)
+        new_height = self.rect.height * round(image_scale)
+        self.image = transform.scale(self.image, (new_width, new_height))
+
         self.rect.center = [x, y]
 
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
 class Player(Vehicle):
-    def __init__(self, x, y):
-        image = image.load('images/car.png')
-        super().__init__(image, x, y)
+    def __init__(self, image_name, x, y):
+        super().__init__(image_name, x, y)
 
